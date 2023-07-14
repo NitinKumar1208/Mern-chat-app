053093f439d4df9867a49e3bdc5343a9bf9234f3
NitinKumar1208 <mehtanitin552@gmail.com>
2023-07-14 21:27:17 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("Api is running")
+  res.send("Nitin is working succesfully");
 });
 
-app.listen(5000, console.log("Server started on 5000"));
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //console.log(req.params.id);
+  const singlechat = chats.find((c) => c._id === req.params.id);
+  res.send(singlechat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log("server started on PORT ${PORT}"));
